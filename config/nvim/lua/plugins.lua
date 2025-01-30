034886adcf3b6008f6b00e307e878bcbebde7dc1
nexus <admin@nexus.local>
2025-01-29 22:00:38 -0500

    nvim-tree added

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -8,6 +8,27 @@ return {
       -- Puedes configurar otros servidores aquí
     end
   },
-  
-}
 
+  -- Plugin para la barra lateral (Nvim-Tree)
+  {
+    "nvim-tree/nvim-tree.lua",
+    dependencies = { "nvim-tree/nvim-web-devicons" }, -- Íconos para archivos
+    config = function()
+      require("nvim-tree").setup({
+        view = {
+          width = 30, -- Ancho de la barra lateral
+          side = "left", -- Posición a la izquierda
+        },
+        filters = {
+          dotfiles = false, -- Mostrar archivos ocultos
+        },
+        git = {
+          enable = true, -- Habilitar integración con git
+        },
+      })
+
+      -- Atajo para abrir/cerrar la barra lateral con <leader>e
+      vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { silent = true, noremap = true })
+    end
+  },
+}
